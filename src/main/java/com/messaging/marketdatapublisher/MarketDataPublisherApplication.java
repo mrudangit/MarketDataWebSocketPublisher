4dc260e936d759828b94a7529684673ca765de39
mrudangit <mrudang@yahoo.com>
2019-10-18 19:40:16 -0400

    Initial

---
+++ src/main/java/com/messaging/marketdatapublisher/MarketDataPublisherApplication.java
@@ -0,0 +1,13 @@
+package com.messaging.marketdatapublisher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketDataPublisherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarketDataPublisherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/messaging/marketdatapublisher/MarketDataPublisherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.messaging.marketdatapublisher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketDataPublisherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
